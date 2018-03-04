ce3771519a75585473b3886de5d98eecec79d5fa
FinchyG <lagaunt120@yahoo.com>
2018-03-04 14:28:41 +0000

    working get answer button - with individual state

--- src/FlagQuestion.js
+++ src/FlagQuestion.js
@@ -4,20 +4,27 @@ export default class FlagQuestion extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {visibility: "hidden"};
+        this.state = {answer: "hidden"};
         this.getAnswer = this.getAnswer.bind(this);
     }
 
     getAnswer() {
-        this.setState({visibility: "visible"});
+        this.setState({answer: "visible"});
     }
 
+/*    checkAnswer() {
+        let answer = document.getElementsByTagName("input").values().trim();
+        if(answer === {this.props.FlagName}{
+            this.setState
+        })
+    }
+*/
     render() {
         return (
         <div>
             <img src={this.props.src} alt="Flag" />
             <input type="text" size="15" />
-            <p className="FlagAnswer" style={{visibility: this.state.visibility}}>{this.props.FlagName}</p>
+            <p className="FlagAnswer" style={{visibility: this.state.answer}}>{this.props.FlagName}</p>
             <span>
               <button>Check Answer</button>
               <button onClick={this.getAnswer}>Get Answer</button>
